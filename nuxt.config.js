0efd9f1c1f1e708e82afe5aebd61210295db62fe
leonhail <leonhail.paypa@positivegroup.com.au>
2021-06-12 11:28:10 +0700

    fix issues

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/vue-notification.js', ssr: false },
   ],
 
   // axios configuration
