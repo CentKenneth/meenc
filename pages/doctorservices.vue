b13f2d0963da1c7b6c15c3cc6d2639ceafa748fb
leonhail <leonhail.paypa@nodifi.com.au>
2022-03-01 19:51:28 +0800

    fix issues

--- pages/doctorservices.vue
+++ pages/doctorservices.vue
@@ -1,6 +1,6 @@
-<template
->
-<div class="pa-0">
+<template>
+  <div class="pa-0">
+    
     <v-app-bar
       class="mx-auto my-1"
       color="#01579B"
@@ -8,290 +8,198 @@
     >
     <v-app-bar-nav-icon @click="$router.push('/doctorhome')"> <v-icon color="white">mdi-arrow-left</v-icon> </v-app-bar-nav-icon>
     <v-spacer></v-spacer>
-       <v-toolbar-title >
-               <img
-                height="40"  
-                max-width="50"
-                src="~assets/M.svg"
-                >
-               <img>
-      </v-toolbar-title>
-          <v-alert
-              color="#01579B"
-              dark
-              dense>
-              MEENC HEALTH CARE
-          </v-alert>
+    <v-alert
+        color="#01579B"
+        dark
+        dense>
+        MEENC HEALTH CARE
+    </v-alert>
     <v-spacer></v-spacer>
     </v-app-bar>
 
-     <v-card flat color="#BBDEFB" class="pa-6">
+    <v-card flat color="#BBDEFB" class="pa-4">
 
-    <v-card-title class="d-flex justify-center">
-      <img
+      <v-card-title class="d-flex justify-center">
+        <img
           class="d-flex justify-center"
           height="50"  
           max-width="50"
-          src="~assets/icon/services.png"
-      ><img>
-      Services
+          src="~assets/icon/services.png">
+        <img>
+        Services
       </v-card-title>
-    <v-card-text :style="$vuetify.breakpoint.smAndDown ? 'width:100%;' : 'width:35%;'" class="mx-auto">
-    </v-card-text>
-    <v-card-actions class="d-flex flex-column my-0 py-0">
-    </v-card-actions>
+
+      <v-card-text
+        @click="$router.push('/doctorviewcpatientconcern')"
+        class="ma-0 pa-4 rounded-lg"
+        color="#BBDEFB" style="border: 2px solid #1976d2; cursor:pointer;">
+
+        <img
+          height="50"  
+          max-width="50"
+          src="~assets/icon/consult.png" />
+
+        <v-btn 
+          class="pa-0 mt-n2"
+          text
+          color="#0277BD"  
+          @click="$router.push('/doctorviewcpatientconcern')">
+          Online Schedule
+        </v-btn> 
+
+        <div class="caption black--text">
+          View patients Online consultation
+        </div>
+
+      </v-card-text>
+
+      <v-card-text
+        @click="$router.push('/doctorviewcpatientconcernfacetoface')"
+        class="ma-0 pa-4 rounded-lg my-4"
+        color="#BBDEFB" style="border: 2px solid #1976d2; cursor:pointer;">
+
+        <img
+          height="50"  
+          max-width="50"
+          src="~assets/icon/consult.png" />
+
+        <v-btn 
+          class="pa-0 mt-n2"
+          text
+          color="#0277BD"  
+          @click="$router.push('/doctorviewcpatientconcernfacetoface')">
+          Face to Face Schedule
+        </v-btn> 
+
+        <div class="caption black--text">
+          View patients Online Face to Face Consultation
+        </div>
+
+      </v-card-text>
+
+      <v-card-text
+        @click="$router.push('/doctorviewpaymentbills')"
+        class="ma-0 pa-4 rounded-lg my-4"
+        color="#BBDEFB" style="border: 2px solid #1976d2; cursor:pointer;">
+
+        <img
+          height="50"  
+          max-width="50"
+          src="~assets/icon/rb.png" />
+
+        <v-btn 
+          class="pa-0 mt-n2"
+          text
+          color="#0277BD"  
+          @click="$router.push('/doctorviewpaymentbills')">
+          Payment Bills
+        </v-btn> 
+
+        <div class="caption black--text">
+          View patients payments
+        </div>
+
+      </v-card-text>
+
+      <v-card-text
+        @click="$router.push('/doctormedicalconcern')"
+        class="ma-0 pa-4 rounded-lg my-4"
+        color="#BBDEFB" style="border: 2px solid #1976d2; cursor:pointer;">
+
+        <img
+          height="50"  
+          max-width="50"
+          src="~assets/icon/chat.png" />
+
+        <v-btn 
+          class="pa-0 mt-n2"
+          text
+          color="#0277BD"  
+          @click="$router.push('/doctormedicalconcern')">
+          Medical concerns
+        </v-btn> 
+
+        <div class="caption black--text">
+          Send medical concerns, conditions and vital signs for your doctor
+        </div>
+
+      </v-card-text>
+
+      <v-card-text
+        @click="$router.push('/doctorcreateprescription')"
+        class="ma-0 pa-4 rounded-lg my-4"
+        color="#BBDEFB" style="border: 2px solid #1976d2; cursor:pointer;">
+
+        <img
+          height="50"  
+          max-width="50"
+          src="~assets/icon/prescription.png" />
+
+        <v-btn 
+          class="pa-0 mt-n2"
+          text
+          color="#0277BD"  
+          @click="$router.push('/doctorcreateprescription')">
+          Create prescription
+        </v-btn> 
+
+        <div class="caption black--text">
+          Create electronic prescription to patient
+        </div>
+
+      </v-card-text>
+
+      <v-card-text
+        @click="$router.push('/doctorschedule')"
+        class="ma-0 pa-4 rounded-lg my-4"
+        color="#BBDEFB" style="border: 2px solid #1976d2; cursor:pointer;">
+
+        <img
+          height="50"  
+          max-width="50"
+          src="~assets/icon/appointment.png" />
+
+        <v-btn 
+          class="pa-0 mt-n2"
+          text
+          color="#0277BD"  
+          @click="$router.push('/doctorschedule')">
+          Add Appointment slot
+        </v-btn> 
+
+        <div class="caption black--text">
+          View appoint schedule for Online Appointment
+        </div>
+
+      </v-card-text>
+
+      <v-card-text
+        @click="$router.push('/doctoreFtoFSchedule')"
+        class="ma-0 pa-4 rounded-lg my-4"
+        color="#BBDEFB" style="border: 2px solid #1976d2; cursor:pointer;">
+
+        <img
+          height="50"  
+          max-width="50"
+          src="~assets/icon/appointment.png" />
+
+        <v-btn 
+          class="pa-0 mt-n2"
+          text
+          color="#0277BD"  
+          @click="$router.push('/doctoreFtoFSchedule')">
+          Face to Face Slot
+        </v-btn> 
+
+        <div class="caption black--text">
+          View appoint schedule for face to face consultation
+        </div>
+
+      </v-card-text>
+
     </v-card>
-    <v-card
-    :max-width="$vuetify.breakpoint.smAndDown ? '100%' : '100%'"
-      class="mx-auto"
-      color="#01579B"
-    >
-    <v-card
-    color="#BBDEFB">
-      <v-row dense>
-        <v-col cols="12">
-
-
-
-          
-<v-card-text :style="$vuetify.breakpoint.smAndDown ? 'width:100%;' : 'width:35%;'" class="mx-auto">
-
-         <v-alert
-            outlined
-            color="#0277BD"
-          >
-            <div class="patients">
-              <v-card-actions>
-                 <div>   
-                 <img
-                 class="d-flex justify-center"
-                    height="50"  
-                    max-width="50"
-                    src="~assets/icon/consult.png"
-                ><img>
-                 </div> 
-                  <v-btn 
-                  text
-                  color="#0277BD"  
-                  @click="$router.push('/doctorviewcpatientconcern')">
-                    Online Schedule
-                  </v-btn> 
-             </v-card-actions>
-             <v-card-subtitle>View patients Online consultation</v-card-subtitle>
-            </div>
-          </v-alert>
-
-          <v-alert
-            outlined
-            color="#0277BD"
-          >
-            <div class="patients">
-              <v-card-actions>
-                 <div>   
-                 <img
-                 class="d-flex justify-center"
-                    height="50"  
-                    max-width="50"
-                    src="~assets/icon/consult.png"
-                ><img>
-                 </div> 
-                  <v-btn 
-                  text
-                  color="#0277BD"  
-                  @click="$router.push('/doctorviewcpatientconcernfacetoface')">
-                    Face to Face Schedule
-                  </v-btn> 
-             </v-card-actions>
-             <v-card-subtitle>View patients Online Face to Face Consultation</v-card-subtitle>
-            </div>
-          </v-alert>
-
-          <v-alert
-            outlined
-            color="#0277BD"
-          >
-            <div class="patients">
-              <v-card-actions>
-                 <div>   
-                 <img
-                 class="d-flex justify-center"
-                    height="50"  
-                    max-width="50"
-                    src="~assets/icon/rb.png"
-                ><img>
-                 </div> 
-                  <v-btn 
-                  text
-                  color="#0277BD"  
-                  @click="$router.push('/doctorviewpaymentbills')">
-                    Payment Bills
-                  </v-btn> 
-             </v-card-actions>
-             <v-card-subtitle>View patients payments</v-card-subtitle>
-            </div>
-          </v-alert>
-
-        <v-alert
-            outlined
-            color="#0277BD"
-          >
-            <div class="patients">
-              <v-card-actions>
-                 <div>   
-                 <img
-                 class="d-flex justify-center"
-                    height="50"  
-                    max-width="50"
-                    src="~assets/icon/chat.png"
-                ><img>
-                 </div> 
-                  <v-btn 
-                  text
-                  color="#0277BD"  
-                  @click="$router.push('/doctormedicalconcern')">
-                    Medical concerns
-                  </v-btn> 
-             </v-card-actions>
-             <v-card-subtitle>Send medical concerns, conditions and vital signs for your doctor</v-card-subtitle>
-            </div>
-          </v-alert>
-          <v-alert
-            outlined
-            color="#0277BD"
-          >
-            <div class="patients">
-              <v-card-actions>
-                 <div>   
-                 <img
-                 class="d-flex justify-center"
-                    height="50"  
-                    max-width="50"
-                    src="~assets/icon/prescription.png"
-                ><img>
-                 </div>   
-                  <v-btn 
-                  text
-                  color="#0277BD"  
-                  @click="$router.push('/doctorcreateprescription')">
-                   Create prescription
-                  </v-btn>
-             </v-card-actions>
-             <v-card-subtitle>Create electronic prescription to patient</v-card-subtitle>
-            </div>
-          </v-alert>
-
-           <!-- <v-alert
-            outlined
-            color="#0277BD"
-          >
-            <div class="patients">
-              <v-card-actions>
-                 <div>   
-                 <img
-                 class="d-flex justify-center"
-                    height="50"  
-                    max-width="50"
-                    src="~assets/icon/prescription.png"
-                ><img>
-                 </div>   
-                  <v-btn 
-                  text
-                  color="#0277BD"  
-                  @click="$router.push('/doctorprescription')">
-                   Send prescription
-                  </v-btn>
-             </v-card-actions>
-             <v-card-subtitle>Provide electronic prescription to patient</v-card-subtitle>
-            </div>
-          </v-alert> -->
-
-          <v-alert
-            outlined
-            color="#0277BD"
-          >
-            <div class="Appointment Schedule">
-              <v-card-actions>
-                
-                  <div> 
-                  <img
-                   class="d-flex justify-center"
-                    height="50"  
-                    max-width="50"    
-                    src="~assets/icon/appointment.png"
-                ><img>
-                </div> 
-                  <v-btn 
-                  text
-                  color="#0277BD"  
-                  @click="$router.push('/doctorschedule')">
-                   Add Appointment slot
-                  </v-btn>  
-             </v-card-actions>
-             <v-card-subtitle>View appoint schedule for Online Appointment</v-card-subtitle>
-            </div>
-          </v-alert>
-
-          <v-alert
-            outlined
-            color="#0277BD"
-          >
-            <div class="Appointment Schedule">
-              <v-card-actions>
-                
-                  <div> 
-                  <img
-                   class="d-flex justify-center"
-                    height="50"  
-                    max-width="50"    
-                    src="~assets/icon/appointment.png"
-                ><img>
-                </div> 
-                  <v-btn 
-                  text
-                  color="#0277BD"  
-                  @click="$router.push('/doctoreFtoFSchedule')">
-                   Face to Face Slot
-                  </v-btn>  
-             </v-card-actions>
-             <v-card-subtitle>View appoint schedule for face to face consultation</v-card-subtitle>
-            </div>
-          </v-alert>
- 
-          <!-- <v-alert
-            outlined
-            color="#0277BD"
-          >
-            <div class="patients">
-              <v-card-actions>
-                  <img
-                  class="d-flex justify-center"
-                    height="50"  
-                    max-width="50"
-                    
-                    src="~assets/icon/history.png"
-                ><img>
-                  <v-btn 
-                  text
-                  color="#0277BD"  
-                  @click="$router.push('/doctormedicalhistory')">
-                    Medical History
-                  </v-btn>
-             </v-card-actions>
-             <v-card-subtitle>Set schedule for online checkup</v-card-subtitle>
-            </div>
-          </v-alert> -->
-</v-card-text>
-
-
-        </v-col>
-      </v-row>
-    </v-card>
-  </v-card>
-
 
   </div>
-
-
 </template>
 <script>
   export default {
--- pages/patientservices.vue
+++ pages/patientservices.vue
@@ -1,9 +1,6 @@
 <template
 >
-<v-container
-    class="spacing-playground pa-0"
-    fluid 
-  >
+  <div>
     <v-app-bar
       class="mx-auto my-1"
       color="#01579B"
@@ -11,14 +8,6 @@
     >
           <v-app-bar-nav-icon @click="$router.push('/patienthome')"> <v-icon color="white">mdi-arrow-left</v-icon> </v-app-bar-nav-icon>
     <v-spacer></v-spacer>
-       <v-toolbar-title >
-               <img
-                height="40"  
-                max-width="50"
-                src="~assets/M.svg"
-                >
-               <img>
-      </v-toolbar-title>
           <v-alert
               color="#01579B"
               dark
@@ -28,256 +17,47 @@
     <v-spacer></v-spacer>
     </v-app-bar>
 
-     <v-card flat color="#BBDEFB" class="pa-6">
-  
+    <v-card flat color="#BBDEFB" class="pa-4">
 
-    <v-card-title class="d-flex justify-center">
-      <img
+      <v-card-title class="d-flex justify-center">
+        <img
           class="d-flex justify-center"
           height="50"  
           max-width="50"
-          src="~assets/icon/services.png"
-      ><img>
-      Services
-    </v-card-title>
+          src="~assets/icon/services.png">
+        <img>
+        Services
+      </v-card-title>
 
-    <v-card-text :style="$vuetify.breakpoint.smAndDown ? 'width:100%;' : 'width:35%;'" class="mx-auto">
-   
+      <v-card-text
+        v-for="(service, index) in services"
+        :key="index"
+        @click="$router.push(service.url)"
+        class="ma-0 pa-4 rounded-lg my-2"
+        color="#BBDEFB" style="border: 2px solid #1976d2; cursor:pointer;">
 
-    </v-card-text>
+        <img
+          height="50"  
+          max-width="50"
+          :src="require(`~/assets/icon/${service.img}.png`)" />
 
-    <v-card-actions class="d-flex flex-column my-0 py-0">
-       
+        <v-btn 
+          class="pa-0 mt-n2"
+          text
+          color="#0277BD"  
+          @click="$router.push(service.url)">
+          {{ service.title }}
+        </v-btn> 
 
-    </v-card-actions>
+        <div class="caption black--text">
+          {{ service.subtitle }}
+        </div>
+
+      </v-card-text>
 
     </v-card>
 
-    <v-card
-    :max-width="$vuetify.breakpoint.smAndDown ? '100%' : '100%'"
-      class="mx-auto"
-      color="#01579B"
-    >
-    <v-card
-    color="#BBDEFB">
-      <v-row dense>
-        <v-col cols="12">
-
-
-
-          
-<v-card-text :style="$vuetify.breakpoint.smAndDown ? 'width:100%;' : 'width:35%;'" class="mx-auto">
-
-         <v-alert
-            outlined
-            color="#0277BD"
-          >
-            <div class="patients">
-              <v-card-actions>
-                 <div>   
-                 <img
-                 class="d-flex justify-center"
-                    height="50"  
-                    max-width="50"
-                    src="~assets/icon/schedule.png"
-                ><img>
-                 </div> 
-                  <v-btn 
-                  text
-                  color="#0277BD" 
-                  @click="$router.push('/patientschedule')">
-                    Online checkup
-                  </v-btn> 
-             </v-card-actions>
-             <v-card-subtitle>Set schedule for online checkup</v-card-subtitle>
-            </div>
-          </v-alert>
-
-          <v-alert
-            outlined
-            color="#0277BD"
-          >
-            <div class="patients">
-              <v-card-actions>
-                 <div>   
-                 <img
-                 class="d-flex justify-center"
-                    height="50"  
-                    max-width="50"
-                    src="~assets/icon/schedule.png"
-                ><img>
-                 </div> 
-                  <v-btn 
-                  text
-                  color="#0277BD" 
-                  @click="$router.push('/patientschedulefacetoface')">
-                    Face to Face Appointment
-                  </v-btn> 
-             </v-card-actions>
-             <v-card-subtitle>Set schedule for Face to Face Appointment</v-card-subtitle>
-            </div>
-          </v-alert>
-
-        <v-alert
-            outlined
-            color="#0277BD"
-          >
-            <div class="patients">
-              <v-card-actions>
-                 <div>   
-                 <img
-                 class="d-flex justify-center"
-                    height="50"  
-                    max-width="50"
-                    src="~assets/icon/chat.png"
-                ><img>
-                 </div> 
-                  <v-btn 
-                  text
-                  color="#0277BD"  
-                  @click="$router.push('/patientmedicalconcern')">
-                    Medical concerns
-                  </v-btn> 
-             </v-card-actions>
-             <v-card-subtitle>Send medical concerns, conditions and vital signs for your doctor</v-card-subtitle>
-            </div>
-          </v-alert>
-
-
-          <v-alert
-            outlined
-            color="#0277BD"
-          >
-            <div class="patients">
-              <v-card-actions>
-                 <div>   
-                 <img
-                 class="d-flex justify-center"
-                    height="50"  
-                    max-width="50"
-                    src="~assets/icon/prescription.png"
-                ><img>
-                 </div>   
-                  <v-btn 
-                  text
-                  color="#0277BD" 
-                  @click="$router.push('/patientprescription')">
-                   Doctor's prescription
-                  </v-btn>
-             </v-card-actions>
-             <v-card-subtitle>View the doctor’s diagnosis and instruction</v-card-subtitle>
-            </div>
-          </v-alert>
-
-          <!-- <v-alert
-            outlined
-            color="#0277BD"
-          >
-            <div class="patients">
-              <v-card-actions>
-                  <img
-                  class="d-flex justify-center"
-                    height="50"  
-                    max-width="50"
-                    
-                    src="~assets/icon/history.png"
-                ><img>
-                  <v-btn 
-                  text
-                  color="#0277BD"  
-                  @click="$router.push('/patientmedicalhistory')">
-                    Medical History
-                  </v-btn>
-             </v-card-actions>
-             <v-card-subtitle>Patient medical history</v-card-subtitle>
-            </div>
-          </v-alert> -->
-
-          <v-alert
-            outlined
-            color="#0277BD"
-            >
-            <div class="patients">
-              <v-card-actions>
-                  <img
-                  class="d-flex justify-center"
-                    height="50"  
-                    max-width="50"
-                    
-                    src="~assets/icon/ma.png"
-                ><img>
-                  <v-btn 
-                  text
-                  color="#0277BD"  
-                  @click="$router.push('/patientsummaryappointments')">
-                    My Appointments
-                  </v-btn>
-             </v-card-actions>
-             <v-card-subtitle>Summary of appointments</v-card-subtitle>
-            </div>
-          </v-alert>
-
-          <v-alert
-            outlined
-            color="#0277BD"
-            >
-            <div class="patients">
-              <v-card-actions>
-                  <img
-                  class="d-flex justify-center"
-                    height="50"  
-                    max-width="50"
-                    
-                    src="~assets/icon/ma.png"
-                ><img>
-                  <v-btn 
-                  text
-                  color="#0277BD"  
-                  @click="$router.push('/patientsummaryappointmentsfacetoface')">
-                    My Face to Face Appointments
-                  </v-btn>
-             </v-card-actions>
-             <v-card-subtitle>Summary of appointments</v-card-subtitle>
-            </div>
-          </v-alert>
-
-          <!-- <v-alert
-            outlined
-            color="#0277BD"
-          >
-            <div class="patients">
-              <v-card-actions>
-                  <img
-                  class="d-flex justify-center"
-                    height="50"  
-                    max-width="50"
-                    
-                    src="~assets/icon/rb.png"
-                ><img>
-                  <v-btn 
-                  text
-                  color="#0277BD"  
-                  @click="$router.push('/patientbillingstatement')">
-                    Recent Payment Bills
-                  </v-btn>
-             </v-card-actions>
-             <v-card-subtitle>Billing statement</v-card-subtitle>
-            </div>
-          </v-alert> -->
-
-          
-</v-card-text>
-
-
-        </v-col>
-      </v-row>
-    </v-card>
-  </v-card>
-
-
-  </v-container>
-
-
+  </div>
 </template>
 <script>
   export default {
@@ -289,10 +69,47 @@
       }
     },
     data: () => ({
+      services: [
+        {
+          title: 'Online checkup',
+          subtitle: 'Set schedule for online checkup',
+          url: '/patientschedule',
+          img: 'schedule'
+        },
+        {
+          title: 'Face to Face Appointment',
+          subtitle: 'Set schedule for Face to Face Appointment',
+          url: '/patientschedulefacetoface',
+          img: 'schedule'
+        },
+        {
+          title: 'Medical concerns',
+          subtitle: 'Send medical concerns, conditions and vital signs for your doctor',
+          url: '/patientmedicalconcern',
+          img: 'chat'
+        },
+        {
+          title: 'Doctors prescription',
+          subtitle: 'View the doctor’s diagnosis and instruction',
+          url: '/patientprescription',
+          img: 'prescription'
+        },
+        {
+          title: 'My Appointments',
+          subtitle: 'Summary of appointments',
+          url: '/patientsummaryappointments',
+          img: 'ma'
+        },
+        {
+          title: 'My Face to Face Appointments',
+          subtitle: 'My Face to Face Appointments',
+          url: '/patientsummaryappointmentsfacetoface',
+          img: 'ma'
+        }
+      ],
       drawer: false,
       group: null,
-    }),
-    data: () => ({ value: 1 }),
+    })
   
   }
 </script>
